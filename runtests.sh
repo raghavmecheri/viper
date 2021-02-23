4d646b2c4f5f749a44a00180f95e59b60f74053d
Raghav Mecheri (Linux) <raghav@mecheri.in>
2021-02-23 04:06:18 +0000

    Super saiyan runtests.sh

--- runtests.sh
+++ runtests.sh
@@ -1,7 +1,21 @@
+verbose=0
+
+while getopts v: flag
+do
+    case "${flag}" in
+        v) verbose=${OPTARG};;
+    esac
+done
+
 cd test/samples
 for i in *.vp; do
     echo "Running test on: $i"
-    ../../viper.native -a $i
+    if [ $verbose -eq 1 ];
+    then
+        ../../viper.native -a $i
+    else
+        ../../viper.native -a $i >/dev/null 2>&1
+    fi
     if [ $? -eq 0 ]
     then
         echo "PASSED"
