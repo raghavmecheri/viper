d060be5fa5627e5d387aff6f026331042fa4dac1
Raghav Mecheri (Linux) <raghav@mecheri.in>
2021-02-23 03:55:39 +0000

    Modified test script to bail on first failure

--- runtests.sh
+++ runtests.sh
@@ -2,5 +2,12 @@ cd test/samples
 for i in *.vp; do
     echo "Running test on: $i"
     ../../viper.native -a $i
+    if [ $? -eq 0 ]
+    then
+        echo "PASSED"
+    else
+        echo "FAILURE: $i"
+        exit 1
+    fi
     echo "______________________________________"
 done
