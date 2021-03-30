8b938ff627590ee22630bd341074cad83dcce289
Tommy Gomez <tjk2132@columbia.edu>
2021-03-29 18:22:12 -0700

    finally got blocks to work

--- decs.ml
+++ decs.ml
@@ -12,14 +12,22 @@ type symbol_table = {
   parent : symbol_table option;
 }
 
+let rec get_stmt_decs stmt scope =
+  match stmt with
+    Block(stmt_list) -> 
+      let new_scope = {
+        variables = StringMap.empty;
+        parent = Some(scope);
+      } in List.fold_left (fun t s -> get_stmt_decs s t) new_scope stmt_list
+  | _ -> scope
+
 let check_decs (stmts, funcs) = 
 
-  let rec find_variable name (scope : symbol_table) = 
-    try
-      StringMap.find name scope.variables
-    with Not_found ->
-      match scope.parent with
-        Some(parent) -> find_variable name parent
-      |  _ -> raise Not_found
+  let rec dup_var name (scope : symbol_table) = 
+    if StringMap.mem name scope.variables then
+      raise (Failure ("Error: " ^ " is already  defined"))
+    else match scope.parent with
+        Some(parent) -> dup_var name parent
+      | _ -> ()
 
 in (stmts, funcs)
