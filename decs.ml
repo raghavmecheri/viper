ac85904772d086155f028f6be4a3c1468b3f8903
Tommy Gomez <tjk2132@columbia.edu>
2021-03-31 14:07:43 -0700

    add support for foriter and while'

--- decs.ml
+++ decs.ml
@@ -36,16 +36,21 @@ let rec get_stmt_decs scope stmt =
     parent = Some(scope);
   } in 
   match stmt with
-    Block(stmt_list) -> 
-      let _ = List.fold_left get_stmt_decs new_scope stmt_list in scope
+    Block(s_list) -> 
+      let _ = List.fold_left get_stmt_decs new_scope s_list in scope
   | Expr(e) -> get_expr_decs scope e
   | Dec(ty, name) -> add_symbol name ty scope
-  | If(cond, then_stmt, else_stmt) -> 
+  | If(cond, then_s, else_s) -> 
       let cond_scope = get_expr_decs new_scope cond in
-      let _ = (get_stmt_decs cond_scope then_stmt, get_stmt_decs cond_scope else_stmt) in scope
+      let _ = (get_stmt_decs cond_scope then_s, get_stmt_decs cond_scope else_s) in scope
   | For(e1, e2, e3, s) -> let expr_list = [e1; e2; e3] in
       let for_scope = List.fold_left get_expr_decs new_scope expr_list in
       let _ = get_stmt_decs for_scope s in scope
+  | DecForIter(ty, name, e, s) -> let iter_scope = add_symbol name ty new_scope in
+      let for_scope = get_expr_decs iter_scope e in
+      let _ = get_stmt_decs for_scope s in scope
+  | While(e, s) -> let while_scope = get_expr_decs new_scope e in
+      let _ = get_stmt_decs while_scope s in scope
   | _ -> scope
 
 let check_decs (stmts, funcs) = 
