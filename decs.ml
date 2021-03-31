182763d7d674f87deb7f21c81f27b0d49b1960ea
Tommy Gomez <tjk2132@columbia.edu>
2021-03-31 14:22:07 -0700

    simplify get_stmt_decs

--- decs.ml
+++ decs.ml
@@ -35,24 +35,24 @@ let rec get_stmt_decs scope stmt =
   let new_scope = {
     variables = StringMap.empty;
     parent = Some(scope);
-  } in 
+  } in let _ =
   match stmt with
-    Block(s_list) -> 
-      let _ = List.fold_left get_stmt_decs new_scope s_list in scope
+    Block(s_list) -> List.fold_left get_stmt_decs new_scope s_list
   | Expr(e) -> get_expr_decs scope e
   | Dec(ty, name) -> add_symbol name ty scope
   | If(cond, then_s, else_s) -> 
       let cond_scope = get_expr_decs new_scope cond in
       let _ = (get_stmt_decs cond_scope then_s, get_stmt_decs cond_scope else_s) in scope
-  | For(e1, e2, e3, s) -> let expr_list = [e1; e2; e3] in
-      let for_scope = List.fold_left get_expr_decs new_scope expr_list in
-      let _ = get_stmt_decs for_scope s in scope
-  | DecForIter(ty, name, e, s) -> let iter_scope = add_symbol name ty new_scope in
-      let for_scope = get_expr_decs iter_scope e in
-      let _ = get_stmt_decs for_scope s in scope
-  | While(e, s) -> let while_scope = get_expr_decs new_scope e in
-      let _ = get_stmt_decs while_scope s in scope
+  | For(e1, e2, e3, s) -> 
+      let expr_list = [e1; e2; e3] in
+      let for_scope = List.fold_left get_expr_decs new_scope expr_list in get_stmt_decs for_scope s
+  | DecForIter(ty, name, e, s) -> 
+      let iter_scope = add_symbol name ty new_scope in
+      let for_scope = get_expr_decs iter_scope e in get_stmt_decs for_scope s
+  | While(e, s) -> 
+      let while_scope = get_expr_decs new_scope e in get_stmt_decs while_scope s
   | _ -> scope
+  in scope
 
 let get_decs (stmts, funcs) = 
   let globals = {
