5af193247d549c0e18ad02c2f011176064a73b35
Tommy Gomez <tjk2132@columbia.edu>
2021-03-31 17:37:08 -0700

    support more expr types

--- decs.ml
+++ decs.ml
@@ -27,17 +27,28 @@ let add_symbol name ty scope = match ty with
           } else scope (* Never enters else clause, but still needed to avoid type error *)
 
 let rec get_expr_decs scope expr = 
-  match expr with
-    DecAssign(ty, name, _) -> add_symbol name ty scope
+  let new_scope = {
+    variables = StringMap.empty;
+    parent = Some(scope);
+  } in match expr with
+    Binop(e1, _, e2) -> 
+      let expr_list = [e1; e2] in List.fold_left get_expr_decs scope expr_list
+  | Unop(_, e) -> get_expr_decs scope e
+  | Ternop(e1, e2, e3) -> 
+      let expr_list = [e1; e2; e3] in List.fold_left get_expr_decs scope expr_list
+  | Assign(_, e) -> get_expr_decs scope e
+  | Deconstruct(b_list, e) -> (* TODO: add case for binds/bind lists *) scope
+  | DecAssign(ty, name, e) -> 
+      let updated_scope = get_expr_decs scope e in add_symbol name ty updated_scope
   | _ -> scope
 
 let rec get_stmt_decs scope stmt =
   let new_scope = {
     variables = StringMap.empty;
     parent = Some(scope);
-  } in let _ =
-  match stmt with
-    Block(s_list) -> List.fold_left get_stmt_decs new_scope s_list
+  } in match stmt with
+    Block(s_list) -> 
+      let _ = List.fold_left get_stmt_decs new_scope s_list in scope
   | Expr(e) -> get_expr_decs scope e
   | Dec(ty, name) -> add_symbol name ty scope
   | If(cond, then_s, else_s) -> 
@@ -45,14 +56,16 @@ let rec get_stmt_decs scope stmt =
       let _ = (get_stmt_decs cond_scope then_s, get_stmt_decs cond_scope else_s) in scope
   | For(e1, e2, e3, s) -> 
       let expr_list = [e1; e2; e3] in
-      let for_scope = List.fold_left get_expr_decs new_scope expr_list in get_stmt_decs for_scope s
+      let for_scope = List.fold_left get_expr_decs new_scope expr_list in 
+      let _ = get_stmt_decs for_scope s in scope
   | DecForIter(ty, name, e, s) -> 
       let iter_scope = add_symbol name ty new_scope in
-      let for_scope = get_expr_decs iter_scope e in get_stmt_decs for_scope s
+      let for_scope = get_expr_decs iter_scope e in 
+      let _ = get_stmt_decs for_scope s in scope
   | While(e, s) -> 
-      let while_scope = get_expr_decs new_scope e in get_stmt_decs while_scope s
+      let while_scope = get_expr_decs new_scope e in 
+      let _ = get_stmt_decs while_scope s in scope
   | _ -> scope
-  in scope
 
 let get_decs (stmts, funcs) = 
   let globals = {
