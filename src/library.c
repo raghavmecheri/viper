930978cf0d06da0e4d2a8b5c192071ceaefc599a
Mustafa <maxusmusti@gmail.com>
2021-04-09 01:52:26 -0400

    Arraylists typed with access + other functions

--- src/library.c
+++ src/library.c
@@ -1,5 +1,181 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <math.h>
+#include <string.h>
+
+struct list{
+    void** data;
+    int size;
+    int mem;
+    char* type;
+};
+
+struct list* create_list(char* type){
+    struct list* inlist = malloc(sizeof(struct list));
+    char* toadd = malloc(strlen(type));
+    strcpy(toadd, type);
+    inlist->type = toadd;
+    inlist->size = 0;
+    inlist->mem = 64;
+    inlist->data = malloc(64);
+    return inlist;
+}
+
+void realloc_check(struct list* inlist){
+    if(inlist->mem <= (inlist->size * 8)){
+        void** newdata = realloc(inlist->data, inlist->mem + 64);
+        if (newdata == NULL){
+            printf("Failure to reallocate data");
+            return;
+        }
+        inlist->data = newdata;
+    }
+}
+
+void append_str(struct list* inlist, char* str){
+    if(strcmp(inlist->type, "string")){
+        printf("Can only append %s, not string\n", inlist->type);
+        return;
+    }
+
+    realloc_check(inlist);
+    char* toadd = malloc(strlen(str));
+    strcpy(toadd, str);
+    inlist->data[(inlist->size)] = toadd;
+    inlist->size = inlist->size + 1;
+}
+
+void append_char(struct list* inlist, char chr){
+    if(strcmp(inlist->type, "char")){
+        printf("Can only append %s, not char\n", inlist->type);
+        return;
+    }
+
+    realloc_check(inlist);
+    char* toadd = malloc(1);
+    *toadd = chr;
+    inlist->data[(inlist->size)] = toadd;
+    inlist->size = inlist->size + 1;
+}
+
+void append_int(struct list* inlist, int num){
+    if(strcmp(inlist->type, "int")){
+        printf("Can only append %s, not int\n", inlist->type);
+        return;
+    }
+
+    realloc_check(inlist);
+    int* toadd = malloc(4);
+    *toadd = num;
+    inlist->data[(inlist->size)] = toadd;
+    inlist->size = inlist->size + 1;
+}
+
+void append_float(struct list* inlist, float flt){
+    if(strcmp(inlist->type, "float")){
+        printf("Can only append %s, not float\n", inlist->type);
+        return;
+    }
+
+    realloc_check(inlist);
+    float* toadd = malloc(4);
+    *toadd = flt;
+    inlist->data[(inlist->size)] = toadd;
+    inlist->size = inlist->size + 1;
+}
+
+void append_list(struct list* inlist, struct list* outlist){
+    if(strcmp(inlist->type, "list")){
+        printf("Can only append %s, not list\n", inlist->type);
+        return;
+    }
+
+    realloc_check(inlist);
+    inlist->data[(inlist->size)] = outlist;
+    inlist->size = inlist->size + 1;
+}
+
+void* access(struct list* inlist, int index){
+    return inlist->data[index];
+}
+
+int access_int(struct list* inlist, int index){
+    if(strcmp(inlist->type, "int")){
+        printf("Can only access %s, not int\n", inlist->type);
+        return 0;
+    }
+
+    int* num = (int*) access(inlist, index);
+    if(num == NULL){
+        printf("Illegal index accessed\n");
+        return 0;
+    }
+    return *num;
+}
+
+char access_char(struct list* inlist, int index){
+    if(strcmp(inlist->type, "char")){
+        printf("Can only access %s, not char\n", inlist->type);
+        return 0;
+    }
+
+    char* chr = (char*) access(inlist, index);
+    if(chr == NULL){
+        printf("Illegal index accessed\n");
+        return 0;
+    }
+    return *chr;
+}
+
+float access_float(struct list* inlist, int index){
+    if(strcmp(inlist->type, "float")){
+        printf("Can only access %s, not float\n", inlist->type);
+        return 0;
+    }
+
+    float* flt = (float*) access(inlist, index);
+    if(flt == NULL){
+        printf("Illegal index accessed\n");
+        return 0;
+    }
+    return *flt;
+}
+
+char* access_str(struct list* inlist, int index){
+    if(strcmp(inlist->type, "string")){
+        printf("Can only access %s, not string\n", inlist->type);
+        return 0;
+    }
+
+    char* str = (char*) access(inlist, index);
+    if(str == NULL){
+        printf("Illegal index accessed\n");
+        return NULL;
+    }
+    return str;
+}
+
+struct list* access_list(struct list* inlist, int index){
+    if(strcmp(inlist->type, "list")){
+        printf("Can only access %s, not list\n", inlist->type);
+        return 0;
+    }
+
+    struct list* lst = (struct list*) access(inlist, index);
+    if(lst == NULL){
+        printf("Illegal index accessed\n");
+        return NULL;
+    }
+    return lst;
+}
+
+char* get_type(struct list* inlist){
+    return inlist->type;
+}
+
+int listlen(struct list* inlist){
+    return inlist->size;
+}
 
 float pow2(float base){
     return pow(base, 2);
@@ -40,4 +216,33 @@ int main(void){
     printf("cmax('k', 'm') = %c\n", cmax('k', 'm'));
     printf("trunc(5.121212) = %f\n", trunc(5.121212));
     printf("ptrunc(5.121212, 3) = %f\n", ptrunc(5.121212, 3));
+
+    struct list* mylist = create_list("int");
+    append_int(mylist, 0);
+    append_str(mylist, "yooo");
+    append_int(mylist, 1);
+    append_int(mylist, 2);
+    append_int(mylist, 3);
+    append_int(mylist, 4);
+    append_int(mylist, 5);
+    append_int(mylist, 6);
+    append_int(mylist, 7);
+    append_int(mylist, 8);
+    //append_float(mylist, 4.5);
+    //append_char(mylist, 'c');
+    struct list* otherlist = create_list("list");
+    //append_int(otherlist, 1);
+    append_list(otherlist, mylist);
+
+    printf("Type: %s\n", get_type(mylist));
+    printf("Type: %s\n", get_type(otherlist));
+    printf("Length: %d\n", listlen(mylist));
+    printf("Length: %d\n", listlen(otherlist));
+
+    printf("@ index 0: %d\n", access_int(mylist, 0));
+    printf("@ index 5: %d\n", access_int(mylist, 5));
+    printf("@ Illegal Index: %d\n", access_int(mylist, 1000));
+
+    struct list* accessed = access_list(otherlist, 0);
+    printf("@ index 5: %d\n", access_int(accessed, 5));
 }
