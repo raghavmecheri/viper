fce3d48ab1eb59b4d25b4c99e52d48532866fa52
Tommy Gomez <tjk2132@columbia.edu>
2021-04-19 12:11:37 -0700

    implement accessassign

--- src/rhandlhand.ml
+++ src/rhandlhand.ml
@@ -1,9 +1,9 @@
 open Sast 
-
+open Ast
 
 let check_decassign ty1 s expr1 = 
     match expr1 with
-        (ty2, _) -> if ty1 = ty2 then (ty1, SDecAssign(ty1, s, expr1)) else raise (Failure "lvalue not equal to rvalue") 
+        (ty2, _) -> if ty1 = ty2 then (ty1, SDecAssign(ty1, s, expr1)) else raise (Failure ("lvalue " ^ string_of_typ ty1 ^ " not equal to rvalue " ^ string_of_typ ty2)) 
       
 
   (*| DecAssign(ty, string, expr1) -> check_assign ty string (expr expr1)*) 
