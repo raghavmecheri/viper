158b94b0c1b9c9218cd969139f5c9f44e08f99bc
Tommy Gomez <tjk2132@columbia.edu>
2021-04-23 14:09:45 -0700

    allow declarations of empty list literals

--- src/rhandlhand.ml
+++ src/rhandlhand.ml
@@ -3,7 +3,9 @@ open Ast
 
 let check_decassign ty1 s expr1 = 
     match expr1 with
-        (ty2, _) -> if ty1 = ty2 then (ty1, SDecAssign(ty1, s, expr1)) else raise (Failure ("lvalue " ^ string_of_typ ty1 ^ " not equal to rvalue " ^ string_of_typ ty2)) 
+        (Array(Nah), SListLiteral([])) -> (ty1, SDecAssign(ty1, s, expr1)) 
+      | (ty2, _) when ty1 = ty2 -> (ty1, SDecAssign(ty1, s, expr1)) 
+      | (ty2, _) -> raise (Failure ("lvalue " ^ string_of_typ ty1 ^ " not equal to rvalue " ^ string_of_typ ty2)) 
       
 
   (*| DecAssign(ty, string, expr1) -> check_assign ty string (expr expr1)*) 
