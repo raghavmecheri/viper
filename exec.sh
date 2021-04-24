6778a50f3cefcec7f1a61d3c5f49610bcdad5752
Raghav Mecheri (Linux) <raghav@mecheri.in>
2021-04-24 22:39:24 +0000

    Fixed exec code

--- exec.sh
+++ exec.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
-
 ./viper.native $1 > a.ll
-cat a.ll
+
+if [ $# -eq 2 ] && [ $2 = "-v" ];
+then
+    cat a.ll
+fi
+
 llc -relocation-model=pic a.ll > a.s
 cc -o a.exe a.s src/library.o -lm
 output=$(./a.exe)
