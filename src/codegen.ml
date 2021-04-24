5a034ad823ae96c7263791aa0f36b1dc5547e907
Raghav Mecheri (Linux) <raghav@mecheri.in>
2021-04-24 20:21:41 +0000

    Jank ternaries + demo update

--- src/codegen.ml
+++ src/codegen.ml
@@ -280,6 +280,25 @@ let translate (_, functions) =
          | A.Decr                 -> raise (Error "Decr not implemented")
         ) e' "tmp" builder
 
+      | STernop(predicate, e1, e2) ->
+        let e1' = expr builder e1 in
+        let e2' = expr builder e2 in
+        (* let bool_val = expr builder predicate in
+        let merge_bb = L.append_block context "merge" the_function in
+        let build_br_merge = L.build_br merge_bb in (* partial function *)
+
+        let then_bb = L.append_block context "then" the_function in
+        add_terminal (expr (L.builder_at_end context then_bb) e1')
+          build_br_merge;
+
+        let else_bb = L.append_block context "else" the_function in
+        add_terminal (expr (L.builder_at_end context else_bb) e2')
+          build_br_merge;
+
+        ignore(L.build_cond_br bool_val then_bb else_bb builder);
+        L.builder_at_end context merge_bb*)
+        e1'
+      
       | SAssign (s, e)            -> 
         let e' = expr builder e in
         ignore(L.build_store e' (lookup s) builder); e'
